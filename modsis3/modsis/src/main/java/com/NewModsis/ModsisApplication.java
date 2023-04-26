38939308f5e59066aa73d2dfe9e5c5fcc0c950ca
emrecan <emrecankahraman17@gmail.com>
2023-04-26 13:25:11 +0300

    .

--- modsis3/modsis/src/main/java/com/NewModsis/ModsisApplication.java
+++ modsis3/modsis/src/main/java/com/NewModsis/ModsisApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ModsisApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModsisApplication.class, args);
